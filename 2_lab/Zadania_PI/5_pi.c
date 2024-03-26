a7c0264bc5a6f081a620904a5f9ced97b7a831db
Irrisorr <zakharkevichg@gmail.com>
2024-03-26 19:10:07 +0100

    completed 5 PI task at 2 lab

---
+++ 2_lab/Zadania_PI/5_pi.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+long long num_steps = 1000000000;
+double step;
+
+int main(int argc, char *argv[])
+{
+    clock_t ppstart, ppstop;
+    double pswtime, pewtime;
+
+    for (int j = 1; j <= 16; j*=2)
+    {
+
+        // volatile
+        double x, pi, sum = 0.0;
+        int i;
+        // RÓWNOLEGLE
+        pswtime = omp_get_wtime();
+        ppstart = clock();
+        sum = 0.0;
+        step = 1. / (double)num_steps;
+
+        #pragma omp parallel reduction (+:sum) num_threads(j)
+        #pragma omp for
+        for (i = 0; i < num_steps; i++)
+        {
+            x = (i + .5) * step;
+            sum = sum + 4.0 / (1. + x * x);
+        }
+
+        pi = sum * step;
+
+        ppstop = clock();
+        pewtime = omp_get_wtime();
+
+        printf(">==========> threads = %d <==========<\n", j);
+        printf("%15.12f Wartosc liczby PI rownolegle \n", pi);
+        printf("Czas procesorów przetwarzania równoleglego  %f sekund \n", ((double)(ppstop - ppstart) / CLOCKS_PER_SEC));
+        printf("Czas trwania obliczen rownoleglych - wallclock %f sekund \n\n", pewtime - pswtime);
+    }
+
+
+    return 0;
+}
--- 2_lab/Zadania_PI/pi.c
+++ 2_lab/Zadania_PI/pi.c
@@ -190,5 +190,42 @@ int main(int argc, char *argv[])
 
 
 
+    // =================================================== 5 zadanie ===================================================
+    printf("----------------------- 5 zadanie -----------------------\n");
+    for (int j = 1; j <= 16; j *= 2)
+    {
+
+        // volatile
+        double x, pi, sum = 0.0;
+        int i;
+        // RÓWNOLEGLE
+        pswtime = omp_get_wtime();
+        ppstart = clock();
+        sum = 0.0;
+        step = 1. / (double)num_steps;
+
+        #pragma omp parallel reduction (+:sum) num_threads(j)
+        #pragma omp for
+        for (i = 0; i < num_steps; i++)
+        {
+            x = (i + .5) * step;
+            sum = sum + 4.0 / (1. + x * x);
+        }
+
+        pi = sum * step;
+
+        ppstop = clock();
+        pewtime = omp_get_wtime();
+
+        printf(">==========> threads = %d <==========<\n", j);
+        printf("%15.12f Wartosc liczby PI rownolegle \n", pi);
+        printf("Czas procesorów przetwarzania równoleglego  %f sekund \n", ((double)(ppstop - ppstart) / CLOCKS_PER_SEC));
+        printf("Czas trwania obliczen rownoleglych - wallclock %f sekund \n", pewtime - pswtime);
+        printf("Przyspieszenie %5.3f \n\n", (sewtime - sswtime) / (pewtime - pswtime));
+    }
+
+    printf("Wykorzystujemy gotową dyrektywę reduction, która automatycznie dodaje lokalne sumy do globalnej.\n\n");
+
+
     return 0;
 }
